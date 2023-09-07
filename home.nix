7644deb4f23ed3597fda787c2ec26e4fe15f8d25
Michael Webb <michaelwebb76@gmail.com>
2023-09-07 19:11:10 +1000

    Do not autostart things

--- home.nix
+++ home.nix
@@ -109,23 +109,9 @@ in
       TERMINAL = "alacritty";
     };
 
-    file = builtins.listToAttrs (map
-      (pkg:
-        {
-          name = ".config/autostart/" + pkg.pname + ".desktop";
-          value =
-            if pkg ? desktopItem then {
-              # Application has a desktopItem entry.
-              # Assume that it was made with makeDesktopEntry, which exposes a
-              # text attribute with the contents of the .desktop file
-              text = pkg.desktopItem.text;
-            } else {
-              # Application does *not* have a desktopItem entry. Try to find a
-              # matching .desktop name in /share/apaplications
-              source = (pkg + "/share/applications/" + pkg.pname + ".desktop");
-            };
-        })
-      autostartPrograms);
+    file.".config/cabal/config".text = ''
+      jobs: $ncpus
+    '';
 
     # Miscellaneous packages (in alphabetical order)
     packages = with pkgs; [
