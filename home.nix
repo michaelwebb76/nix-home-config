38b39a1561e5de911eb2de2a55fe8057ad5e5e10
Michael Webb <michaelwebb76@gmail.com>
2024-02-01 17:29:33 +1100

    partially working with flakes

--- home.nix
+++ home.nix
@@ -1,45 +1,11 @@
-{ config, lib, ... }:
+{ config, pkgs, ... }:
 
 let
-  nixpkgsRev = "23.05";
-  pkgs = import (fetchTarball "https://github.com/nixos/nixpkgs/archive/${nixpkgsRev}.tar.gz") { };
-
   # Import other Nix files
   imports = [
     ./git.nix
-    ./neovim.nix
     ./shell.nix
     ./tmux.nix
-  ];
-
-  # Handly shell command to view the dependency tree of Nix packages
-  depends = pkgs.writeScriptBin "depends" ''
-    dep=$1
-    nix-store --query --requisites $(which $dep)
-  '';
-
-
-  git-hash = pkgs.writeScriptBin "git-hash" ''
-    nix-prefetch-url --unpack https://github.com/$1/$2/archive/$3.tar.gz
-  '';
-
-  wo = pkgs.writeScriptBin "wo" ''
-    readlink $(which $1)
-  '';
-
-  run = pkgs.writeScriptBin "run" ''
-    nix-shell --pure --run "$@"
-  '';
-
-  scripts = [
-    depends
-    git-hash
-    run
-    wo
-  ];
-
-  gitTools = with pkgs.gitAndTools; [
-    gh
   ];
 
   userName = "michaelwebb";
@@ -48,69 +14,40 @@ in
 {
   inherit imports;
 
-  # Allow non-free (as in beer) packages
-  nixpkgs.config = {
-    allowUnfree = true;
-    allowUnsupportedSystem = true;
-  };
-
-  # Enable Home Manager
-  programs = {
-    home-manager.enable = true;
-
-    # Enable direnv
-    direnv = {
-      enable = true;
-      nix-direnv.enable = true;
-    };
-
-    zsh.enable = true;
-    ssh = {
-      controlMaster = "auto";
-      controlPath = "/tmp/ssh_mux_%h_%p_%r";
-      controlPersist = "10";
-      enable = true;
-      extraOptionOverrides = {
-        AddKeysToAgent = "yes";
-        ControlMaster = "auto";
-        IdentityFile = "${homePath}/.ssh/id_rsa";
-        IgnoreUnknown = "UseKeychain";
-        TCPKeepAlive = "yes";
-        UseKeychain = "yes";
-      };
-      forwardAgent = true;
-      matchBlocks = {
-        "bread-staging.trikeapps.com" = {
-          user = "mike";
-        };
-      };
-      serverAliveInterval = 120;
-    };
-  };
-
   home = {
+    # Home Manager needs a bit of information about you and the paths it should
+    # manage.
     username = "${userName}";
     homeDirectory = "${homePath}";
-    stateVersion = "23.05";
-    sessionVariables = {
-      EDITOR = "code";
-      TERMINAL = "alacritty";
-    };
 
-    file.".config/cabal/config".text = ''
-      build-summary: ${homePath}/.cache/cabal/logs/build.log
-      extra-prog-path: ${homePath}/.local/bin
-      installdir: ${homePath}/.local/bin
-      jobs: $ncpus
-      nix: disable
-      remote-build-reporting: none
-      remote-repo-cache: ${homePath}/.cache/cabal/packages
-      repository hackage.haskell.org
-        url: http://hackage.haskell.org/
-    '';
+    # This value determines the Home Manager release that your configuration is
+    # compatible with. This helps avoid breakage when a new Home Manager release
+    # introduces backwards incompatible changes.
+    #
+    # You should not change this value, even if you update Home Manager. If you do
+    # want to update the value, then make sure to first check the Home Manager
+    # release notes.
+    stateVersion = "23.11"; # Please read the comment before changing.
 
-    # Miscellaneous packages (in alphabetical order)
+    # The home.packages option allows you to install Nix packages into your
+    # environment.
     packages = with pkgs; [
+      # # Adds the 'hello' command to your environment. It prints a friendly
+      # # "Hello, world!" when run.
+      # pkgs.hello
+
+      # # It is sometimes useful to fine-tune packages, for example, by applying
+      # # overrides. You can do that directly here, just don't forget the
+      # # parentheses. Maybe you want to install Nerd Fonts with a limited number of
+      # # fonts?
+      # (pkgs.nerdfonts.override { fonts = [ "FantasqueSansMono" ]; })
+
+      # # You can also create simple shell scripts directly inside your
+      # # configuration. For example, this adds a command 'my-hello' to your
+      # # environment:
+      # (pkgs.writeShellScriptBin "my-hello" ''
+      #   echo "Hello, ${config.home.username}!"
+      # '')
       awscli2
       cachix # Nix build cache
       curl # An old classic
@@ -132,6 +69,60 @@ in
       watchman
       wget
       zsh-z
-    ] ++ gitTools ++ scripts;
+    ];
+
+    # Home Manager is pretty good at managing dotfiles. The primary way to manage
+    # plain files is through 'home.file'.
+    file = {
+      ".config/cabal/config".text = ''
+        build-summary: ${homePath}/.cache/cabal/logs/build.log
+        extra-prog-path: ${homePath}/.local/bin
+        installdir: ${homePath}/.local/bin
+        jobs: $ncpus
+        nix: disable
+        remote-build-reporting: none
+        remote-repo-cache: ${homePath}/.cache/cabal/packages
+        repository hackage.haskell.org
+          url: http://hackage.haskell.org/
+      '';
+    };
+
+    # Home Manager can also manage your environment variables through
+    # 'home.sessionVariables'. If you don't want to manage your shell through Home
+    # Manager then you have to manually source 'hm-session-vars.sh' located at
+    # either
+    #
+    #  ~/.nix-profile/etc/profile.d/hm-session-vars.sh
+    #
+    # or
+    #
+    #  ~/.local/state/nix/profiles/profile/etc/profile.d/hm-session-vars.sh
+    #
+    # or
+    #
+    #  /etc/profiles/per-user/michaelwebb/etc/profile.d/hm-session-vars.sh
+    #
+    sessionVariables = {
+      EDITOR = "code";
+      TERMINAL = "alacritty";
+    };
+  };
+
+  # Let Home Manager install and manage itself.
+  programs = {
+    home-manager.enable = true;
+
+    # Enable direnv
+    direnv = {
+      enable = true;
+      nix-direnv.enable = true;
+    };
+
+    zsh.enable = true;
+  };
+
+  nixpkgs.config = {
+    allowUnfree = true;
+    allowUnsupportedSystem = true;
   };
 }
